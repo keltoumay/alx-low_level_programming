8f0b5fc452f6f88e80a6054e70abed9cd4c999d4
keltoumay <keltoumaouyahia@gmail.com>
2023-05-12 18:52:07 -0700

    2. The best argument against democracy is a five-minute conversation with the average voter

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,38 +2,20 @@
 #include "main.h"
 
 /**
- * print_args - prints all arguments in the array
- * @args: array of arguments
- * @num_args: number of arguments in the array
- *
- * Return: void
- */
-void print_args(char **args, int num_args)
-{
-int i;
-
-for (i = 0; i < num_args; i++)
-{
-printf("%s\n", args[i]);
-}
-}
-
-/**
- * main - entry point, calls print_args function to print arguments
+ * main - prints all arguments it receives
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: 0 on success, non-zero on error
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-if (argc < 2)
-{
-printf("Usage: %s [arg1] [arg2] ...\n", argv[0]);
-return (1);
-}
+int i;
 
-print_args(argv + 1, argc - 1);
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
 
 return (0);
 }
