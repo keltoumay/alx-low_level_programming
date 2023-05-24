5d4e9332125198eaf4f51696b9f53783ce64677b
keltoumay <keltoumaouyahia@gmail.com>
2023-05-24 16:22:47 -0700

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,31 +1,26 @@
 #include "3-calc.h"
 #include <stdlib.h>
-
 /**
  * get_op_func - Selects the correct function to perform
- *               the operation asked by the user.
+ * the operation asked by the user.
  * @s: The operator passed as argument.
  *
  * Return: A pointer to the function corresponding
- *         to the operator given as a parameter.
+ * to the operator given as a parameter.
  */
 int (*get_op_func(char *s))(int, int)
 {
 op_t ops[] = {
-{"+", op_add},  /* Operator: Addition */
-{"-", op_sub},  /* Operator: Subtraction */
-{"*", op_mul},  /* Operator: Multiplication */
-{"/", op_div},  /* Operator: Division */
-{"%", op_mod},  /* Operator: Modulo */
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
 {NULL, NULL},
 };
 
 int i = 0;
 
-/**
- * Iterate over the array of operations until the
- * operator is found or the end of the array is reached.
- */
 while (ops[i].op != NULL && *(ops[i].op) != *s)
 i++;
 
