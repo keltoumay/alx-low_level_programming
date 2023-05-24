63834e65c40e7dfb61316e3ba7dd01757156c661
keltoumay <keltoumaouyahia@gmail.com>
2023-05-24 15:33:34 -0700

    2. To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,11 +7,11 @@
  * @cmp: pointer to the comparison function
  *
  * Description: This function searches for an integer in the given array
- *              by applying the comparison function to each element. It
- *              returns the index of the first element for which the
- *              comparison function returns true. If no such element is
- *              found, or if the array or comparison function is NULL, it
- *              returns -1.
+ * by applying the comparison function to each element. It
+ * returns the index of the first element for which the
+ * comparison function returns true. If no such element is
+ * found, or if the array or comparison function is NULL, it
+ * returns -1.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -25,6 +25,5 @@ for (i = 0; i < size; i++)
 if (cmp(array[i]))
 return (i);
 }
-
 return (-1);
 }
