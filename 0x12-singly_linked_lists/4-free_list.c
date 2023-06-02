de183110151ff18876c7605b73e090729b850702
keltoumay <keltoumaouyahia@gmail.com>
2023-06-01 18:14:14 -0700

    4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a linked list.
+ * @head: Pointer to the head of the list.
+ */
+void free_list(list_t *head)
+{
+    if (head == NULL)
+        return;
+
+    /* Recursively free the rest of the list */
+    free_list(head->next);
+
+    /* Free the current node */
+    free(head->str);
+    free(head);
+}
