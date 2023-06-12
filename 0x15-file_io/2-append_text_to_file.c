e72d0af93dd8bc79b9fc1d1e02e37adc83c6837a
keltoumay <keltoumaouyahia@gmail.com>
2023-06-12 15:17:46 -0700

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - function that convert
+ * @filename: a
+ * @text_content: a
+ * Return: Always 0.
+ */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
 int fd, wrlen, close_status;
 /* rw------- */
-mode_t mode = S_IRUSR | S_IWUSR;-
+mode_t mode = S_IRUSR | S_IWUSR;
 
 if (filename == NULL)
 return (-1);
@@ -31,7 +35,8 @@ return (-1);
 
 close_status = close(fd);
 if (close_status == -1)
-return -1;
+
+return (-1);
 
 return (1);
 }
