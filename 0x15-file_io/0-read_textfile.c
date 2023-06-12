79c140ccb0cfe5ad7aed6c8fc269314572109d6d
keltoumay <keltoumaouyahia@gmail.com>
2023-06-12 14:48:14 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,40 +11,40 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t fopen, fread, fwritten;
-    char *buffer;
+ssize_t fopen, fread, fwritten;
+char *buffer;
 
-    if (filename == NULL)
-        return (0);
+if (filename == NULL)
+return (0);
 
-    fopen = open(filename, O_RDONLY);
-    if (fopen == -1)
-        return (0);
+fopen = open(filename, O_RDONLY);
+if (fopen == -1)
+return (0);
 
-    buffer = malloc(sizeof(char) * (letters + 1));
-    if (buffer == NULL)
-    {
-        close(fopen);
-        return (0);
-    }
+buffer = malloc(sizeof(char) * (letters + 1));
+if (buffer == NULL)
+{
+close(fopen);
+return (0);
+}
 
-    fread = read(fopen, buffer, letters);
-    if (fread == -1)
-    {
-        free(buffer);
-        close(fopen);
-        return (0);
-    }
+fread = read(fopen, buffer, letters);
+if (fread == -1)
+{
+free(buffer);
+close(fopen);
+return (0);
+}
 
-    fwritten = write(STDOUT_FILENO, buffer, fread);
-    if (fwritten == -1 || fwritten != fread)
-    {
-        free(buffer);
-        close(fopen);
-        return (0);
-    }
+fwritten = write(STDOUT_FILENO, buffer, fread);
+if (fwritten == -1 || fwritten != fread)
+{
+free(buffer);
+close(fopen);
+return (0);
+}
 
-    free(buffer);
-    close(fopen);
-    return (fwritten);
+free(buffer);
+close(fopen);
+return (fwritten);
 }
