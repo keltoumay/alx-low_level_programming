b3cecbef609f7b6b9151c76f231b32fe0efc6845
keltoumay <keltoumaouyahia@gmail.com>
2023-09-04 13:00:52 -0700

    all updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,11 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * create_file - Create  file.
- * @filename: pointer
- * @text_content: pointer
+ * @filename: pointer.
+ * @text_content: pointer.
  *
- * Return: If the function fails -1
+ * Return: If the function fails -1.
  */
 
 int create_file(const char *filename, char *text_content)
