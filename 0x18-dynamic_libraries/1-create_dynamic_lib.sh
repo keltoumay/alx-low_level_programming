48b270fac38c391df63f4214b13b408d733713a5
keltoumay <keltoumaouyahia@gmail.com>
2023-10-16 11:54:56 -0700

    update

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
-gcc -shared -o liball.so *.o
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+
+// A script that creates a dynamic library called liball.so
+
+gcc *.c -fPIC -shared -o liball.so
